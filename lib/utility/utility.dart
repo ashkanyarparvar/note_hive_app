cdd14dd323e8e5a7ae1a0552bede91c55bf6244f
ashkanyarparvar <ashkanyarparvar@yahoo.com>
2023-10-12 01:46:07 +0330

    upload note hive database on github

---
+++ lib/utility/utility.dart
@@ -0,0 +1,21 @@
+import '../data/type_enum.dart';
+import '../data/task_type.dart';
+
+List<TaskType> getTaskTypeList() {
+  var list = [
+    TaskType(
+        image: 'images/meditate.png',
+        title: 'تمرکز',
+        taskTypeEnum: TaskTypeEnum.foucs),
+    TaskType(
+        image: 'images/social_frends.png',
+        title: 'میتینگ',
+        taskTypeEnum: TaskTypeEnum.date),
+    TaskType(
+        image: 'images/hard_working.png',
+        title: 'کارزیاد',
+        taskTypeEnum: TaskTypeEnum.working),
+  ];
+
+  return list;
+}
